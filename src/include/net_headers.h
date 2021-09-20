183a4a263cb1116b01a51118611c8d8b60e26fe5
Oleg Karbushev <oleg.karbushev@gmail.com>
2021-09-21 02:58:26 +0300

    Adds printf format to print bits

--- src/include/net_headers.h
+++ src/include/net_headers.h
@@ -13,8 +13,8 @@
 
 #endif
 
-#define PRINTF_BINARY_PATTERN_INT8 "%c%c%c%c%c%c%c%c"
-#define PRINTF_BYTE_TO_BINARY_INT8(i)    \
+#define PRINTF_BIN_FMT_INT8 "%c%c%c%c%c%c%c%c"
+#define PRINTF_BINARY_INT8(i)    \
     (((i) & 0x80ll) ? '1' : '0'), \
     (((i) & 0x40ll) ? '1' : '0'), \
     (((i) & 0x20ll) ? '1' : '0'), \
@@ -24,18 +24,18 @@
     (((i) & 0x02ll) ? '1' : '0'), \
     (((i) & 0x01ll) ? '1' : '0')
 
-#define PRINTF_BINARY_PATTERN_INT16 \
-    PRINTF_BINARY_PATTERN_INT8              PRINTF_BINARY_PATTERN_INT8
-#define PRINTF_BYTE_TO_BINARY_INT16(i) \
-    PRINTF_BYTE_TO_BINARY_INT8((i) >> 8),   PRINTF_BYTE_TO_BINARY_INT8(i)
-#define PRINTF_BINARY_PATTERN_INT32 \
-    PRINTF_BINARY_PATTERN_INT16             PRINTF_BINARY_PATTERN_INT16
-#define PRINTF_BYTE_TO_BINARY_INT32(i) \
-    PRINTF_BYTE_TO_BINARY_INT16((i) >> 16), PRINTF_BYTE_TO_BINARY_INT16(i)
-#define PRINTF_BINARY_PATTERN_INT64    \
-    PRINTF_BINARY_PATTERN_INT32             PRINTF_BINARY_PATTERN_INT32
-#define PRINTF_BYTE_TO_BINARY_INT64(i) \
-    PRINTF_BYTE_TO_BINARY_INT32((i) >> 32), PRINTF_BYTE_TO_BINARY_INT32(i)
+#define PRINTF_BIN_FMT_INT16 \
+    PRINTF_BIN_FMT_INT8              PRINTF_BIN_FMT_INT8
+#define PRINTF_BINARY_INT16(i) \
+    PRINTF_BINARY_INT8((i) >> 8),   PRINTF_BINARY_INT8(i)
+#define PRINTF_BIN_FMT_INT32 \
+    PRINTF_BIN_FMT_INT16             PRINTF_BIN_FMT_INT16
+#define PRINTF_BINARY_INT32(i) \
+    PRINTF_BINARY_INT16((i) >> 16), PRINTF_BINARY_INT16(i)
+#define PRINTF_BIN_FMT_INT64    \
+    PRINTF_BIN_FMT_INT32             PRINTF_BIN_FMT_INT32
+#define PRINTF_BINARY_INT64(i) \
+    PRINTF_BINARY_INT32((i) >> 32), PRINTF_BINARY_INT32(i)
 
 
 /* Octets in ethernet address */
